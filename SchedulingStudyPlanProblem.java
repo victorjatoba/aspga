0570b48ee8e81e34fc2a7560930ec55be511358f
Victor Miranda Gonçalves Jatobá <victorjatoba10@gmail.com>
2014-01-15 23:19:44 -0300

    Read Params
    put the params files and init the read logic.

--- SchedulingStudyPlanProblem.java
+++ SchedulingStudyPlanProblem.java
@@ -19,18 +19,110 @@ import ec.simple.*;
  * By: Victor Jatoba
  */
 
+/**
+ * @author victorjatoba
+ * @version 1.0
+ */
+
+
 public class SchedulingStudyPlanProblem extends Problem implements SimpleProblemForm
 {
     private static final long serialVersionUID = 1;
+
+    public static final String P_COURSEINFORMATION = "courseinformation";
+    public static final String P_INTELECTUALAVAIBLE = "intelectualavaible";
+    public static final String P_DAYPERIODAVAIBLE = "dayperiodavaible";
+    public static final String P_STUDENTINFORMATION = "studentinformation";
+
+    public void
 
     public void setup(  final EvolutionState state,
                         final Parameter base)
     {
 
+        File courseInformationInput  = null;
+        File intelectuAlavaibleInput = null;
+        File dayPeriodAvaibleInput   = null;
+        File studentInformationInput = null;
+
+        courseInformationInput  = state.parameters.getFile(base.push(P_COURSEINFORMATION),null);
+        intelectuAlavaibleInput = state.parameters.getFile(base.push(P_INTELECTUALAVAIBLE),null);
+        dayPeriodAvaibleInput   = state.parameters.getFile(base.push(P_DAYPERIODAVAIBLE),null);
+        studentInformationInput = state.parameters.getFile(base.push(P_STUDENTINFORMATION),null);
+
+        verifyInputExistence(courseInformationInput, intelectuAlavaibleInput, dayPeriodAvaibleInput, studentInformationInput);
+
+        convertFileToString(courseinformation);
+        convertFileToString(intelectualavaible);
+        convertFileToString(dayperiodavaible);
+        convertFileToString(studentinformation);
+
     }
 
     /**
-    * implementa a ftness function
+    *   Verify if the inputs are not null.
+    */
+    public void verifyInputExistence(   File courseInformationInput,
+                                        File intelectuAlavaibleInput,
+                                        File dayPeriodAvaibleInput,
+                                        File studentInformationInput)
+    {
+
+        if (courseInformationInput == null) {
+            state.output.error("CourseInformation File doesn't exist", base.push(P_COURSEINFORMATION));
+        }
+        if (intelectuAlavaibleInput == null) {
+            state.output.error("IntelectuAlavaible File doesn't exist", base.push(P_INTELECTUALAVAIBLE));
+        }
+        if (dayPeriodAvaibleInput == null) {
+            state.output.error("DayPeriodAvaible File doesn't exist", base.push(P_DAYPERIODAVAIBLE));
+        }
+        if (studentInformationInput == null) {
+            state.output.error("StudentInformation File doesn't exist", base.push(P_STUDENTINFORMATION));
+        }
+
+        state.output.exitIfErrors();
+    }
+
+    public void convertFileToString(File file) {
+        try {
+
+            inputStream = new FileInputStream(file);
+
+            br = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            System.out.println(sb.toString());
+            System.out.println("\nDone!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+    *   Implement the ftness function
     */
     public void evaluate(   final EvolutionState state,
                             final Individual ind,
