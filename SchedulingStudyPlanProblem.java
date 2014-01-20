83b97a489c2d598789ff2319d98bc835bdda6b7d
Victor Jatobá <victorjatoba10@gmail.com>
2014-01-20 00:09:48 -0300

    #1

--- SchedulingStudyPlanProblem.java
+++ SchedulingStudyPlanProblem.java
@@ -8,9 +8,22 @@
 package ec.app.pea;
 import ec.util.*;
 import ec.*;
-import ec.gp.*;
-import ec.gp.koza.*;
 import ec.simple.*;
+import ec.vector.BitVectorIndividual;
+import ec.util.*; //Parameter
+
+
+//Packages to read
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
 
 /**
  * SchedulingStudyPlanProblem.java
@@ -30,32 +43,35 @@ public class SchedulingStudyPlanProblem extends Problem implements SimpleProblem
     private static final long serialVersionUID = 1;
 
     public static final String P_COURSEINFORMATION = "courseinformation";
-    public static final String P_INTELECTUALAVAIBLE = "intelectualavaible";
-    public static final String P_DAYPERIODAVAIBLE = "dayperiodavaible";
+    public static final String P_INTELECTUALAVAILABLE = "intelectualavailable";
+    public static final String P_DAYPERIODAVAILABLE = "dayperiodavailable";
     public static final String P_STUDENTINFORMATION = "studentinformation";
 
-    public void
+    public EvolutionState state;
+    public Parameter base;
 
-    public void setup(  final EvolutionState state,
-                        final Parameter base)
-    {
+    public void setup (  final EvolutionState state, final Parameter base) {
+        this.state = state;
+        this.base = base;
 
-        File courseInformationInput  = null;
-        File intelectuAlavaibleInput = null;
-        File dayPeriodAvaibleInput   = null;
-        File studentInformationInput = null;
+        File courseInformationInput     = null;
+        File intelectualAvailableInput  = null;
+        File dayPeriodAvailableInput    = null;
+        File studentInformationInput    = null;
 
-        courseInformationInput  = state.parameters.getFile(base.push(P_COURSEINFORMATION),null);
-        intelectuAlavaibleInput = state.parameters.getFile(base.push(P_INTELECTUALAVAIBLE),null);
-        dayPeriodAvaibleInput   = state.parameters.getFile(base.push(P_DAYPERIODAVAIBLE),null);
-        studentInformationInput = state.parameters.getFile(base.push(P_STUDENTINFORMATION),null);
+        courseInformationInput      = state.parameters.getFile(base.push(P_COURSEINFORMATION),null);
+        intelectualAvailableInput   = state.parameters.getFile(base.push(P_INTELECTUALAVAILABLE),null);
+        dayPeriodAvailableInput     = state.parameters.getFile(base.push(P_DAYPERIODAVAILABLE),null);
+        studentInformationInput     = state.parameters.getFile(base.push(P_STUDENTINFORMATION),null);
 
-        verifyInputExistence(courseInformationInput, intelectuAlavaibleInput, dayPeriodAvaibleInput, studentInformationInput);
+        verifyInputExistence(courseInformationInput, intelectualAvailableInput, dayPeriodAvailableInput, studentInformationInput);
 
-        convertFileToString(courseinformation);
-        convertFileToString(intelectualavaible);
-        convertFileToString(dayperiodavaible);
-        convertFileToString(studentinformation);
+        Vector<String> courseInformationVector = convertFileToVectorString(courseInformationInput);
+        Vector<String> intelectuAlavailableVector = convertFileToVectorString(intelectualAvailableInput);
+        Vector<String> dayPeriodAvailableVector = convertFileToVectorString(dayPeriodAvailableInput);
+        Vector<String> studentInformationVector = convertFileToVectorString(studentInformationInput);
+
+        printInput(courseInformationVector, "CourseInformation");
 
     }
 
@@ -63,19 +79,19 @@ public class SchedulingStudyPlanProblem extends Problem implements SimpleProblem
     *   Verify if the inputs are not null.
     */
     public void verifyInputExistence(   File courseInformationInput,
-                                        File intelectuAlavaibleInput,
-                                        File dayPeriodAvaibleInput,
+                                        File intelectualAvailableInput,
+                                        File dayPeriodAvailableInput,
                                         File studentInformationInput)
     {
 
         if (courseInformationInput == null) {
             state.output.error("CourseInformation File doesn't exist", base.push(P_COURSEINFORMATION));
         }
-        if (intelectuAlavaibleInput == null) {
-            state.output.error("IntelectuAlavaible File doesn't exist", base.push(P_INTELECTUALAVAIBLE));
+        if (intelectualAvailableInput == null) {
+            state.output.error("intelectualAvailable File doesn't exist", base.push(P_INTELECTUALAVAILABLE));
         }
-        if (dayPeriodAvaibleInput == null) {
-            state.output.error("DayPeriodAvaible File doesn't exist", base.push(P_DAYPERIODAVAIBLE));
+        if (dayPeriodAvailableInput == null) {
+            state.output.error("DayPeriodAvailable File doesn't exist", base.push(P_DAYPERIODAVAILABLE));
         }
         if (studentInformationInput == null) {
             state.output.error("StudentInformation File doesn't exist", base.push(P_STUDENTINFORMATION));
@@ -84,22 +100,29 @@ public class SchedulingStudyPlanProblem extends Problem implements SimpleProblem
         state.output.exitIfErrors();
     }
 
-    public void convertFileToString(File file) {
+    /**
+    *   Convert a file to a vector of string.
+    */
+    public Vector<String> convertFileToVectorString(File file) {
+
+        FileInputStream inputStream = null;
+        BufferedReader br           = null;
+        Vector<String> lines        = null;
+
         try {
 
             inputStream = new FileInputStream(file);
 
             br = new BufferedReader(new InputStreamReader(inputStream));
 
-            StringBuilder sb = new StringBuilder();
+            //StringBuilder sb = new StringBuilder();
 
             String line;
+            lines = new Vector<String>();
             while ((line = br.readLine()) != null) {
-                sb.append(line);
+                //sb.append(line);
+                lines.add(line);
             }
-
-            System.out.println(sb.toString());
-            System.out.println("\nDone!");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -119,6 +142,24 @@ public class SchedulingStudyPlanProblem extends Problem implements SimpleProblem
                 }
             }
         }
+
+        return lines;
+    }
+
+    /**
+    *   Print in the console the lines of the input.
+    */
+    public void printInput(Vector<String> lines, String type) {
+
+        System.out.println("\n----------------"+ type +"--------------------!");
+
+        for (String lineIt: lines) {
+            state.output.message("courseInformationInput = " + lineIt);
+            System.out.println();
+        }
+
+        System.out.println("\n----------------Done!-------------------");
+
     }
 
     /**
@@ -153,9 +194,11 @@ public class SchedulingStudyPlanProblem extends Problem implements SimpleProblem
         ind2.evaluated = true;
     }
 
-    public float calculateFitnessValue(BitVectorIndividual ind) {
-        ((acepts*100)/qtdFixedConstraints) + ((acepts*100)/qtdFixedConstraints)
+    public float calculateFitnessValue() {
+/*        ((acepts*100)/qtdFixedConstraints) + ((acepts*100)/qtdFixedConstraints)
         (float)(((double)sum)/ind2.genome.length)
+*/
+        return 0.0f;
     }
 
     /**
